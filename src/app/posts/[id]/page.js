070c44c4e50491e833c0fab0c3e90e5c454d7710
abhishek <abhishek.nt94@gmail.com>
2024-06-01 23:11:04 +0530

    Added input section

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Input from '@/components/Input'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className=' mx-auto border-r border-l min-h-screen w-full'>
+    <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+      <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+    </div>
+    <Input />
+  </div>
+    
   )
 }
 
