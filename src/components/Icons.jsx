29ba653de77b7ada9d63252cfac59dfa32100609
abhishek <abhishek.nt94@gmail.com>
2024-06-09 15:17:18 +0530

    Added comment post functionallity

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -15,6 +15,7 @@ export default function Icons({id,uid}) {
     const db = getFirestore(app);
     const [open, setOpen] = useRecoilState(modalState);
     const [postId, setPostId] = useRecoilState(postIdState);
+    const [comments, setComments] = useState([]); 
    
     const likePost = async () => {
         if (session) {
@@ -60,11 +61,21 @@ export default function Icons({id,uid}) {
           likes.findIndex((like) => like.id === session?.user?.uid) !== -1
         );
       }, [likes]);
+
+//comments
+      useEffect(() => {
+        const unsubscribe = onSnapshot(
+          collection(db, 'posts', id, 'comments'),
+          (snapshot) => setComments(snapshot.docs)
+        );
+        return () => unsubscribe();
+      }, [db, id]);
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
-      <HiOutlineChat
-        className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
-        onClick={() => {
+      <div className='flex items-center'>
+        <HiOutlineChat
+          className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
+          onClick={() => {
             if (!session) {
               signIn();
             } else {
@@ -72,7 +83,11 @@ export default function Icons({id,uid}) {
               setPostId(id);
             }
           }}
-      />
+        />
+        {comments.length > 0 && (
+          <span className='text-xs'>{comments.length}</span>
+        )}
+      </div>
       
       <div className='flex items-center'>
         {isLiked ? (
